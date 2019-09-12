a3622fb284873a4baf8d661df3d6ac3a5e30e30c
sihem <digiteck113@gmail.com>
2019-09-12 16:11:41 +0100

    serveur conflit

--- serveur.js
+++ serveur.js
@@ -62,10 +62,5 @@ app.use(auth)
 app.use(require('./routes/adherent.js'))
 
 /** Fin Nos Routes **/
-<<<<<<< HEAD
 
 app.listen(8083);
-
-=======
-app.listen(8083); //Déclaration du port d'écoute de serveur
->>>>>>> 633328f02eb2da1b94168e5d3dbdef401e455924

4c504af72d08fdf15a57ee2348ed83ff1068654a
LeilaGRB <leilaguerrab82@gmail.com>
2019-09-16 00:06:21 +0200

    Test Branch

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: 'eu-cdbr-west-02.cleardb.net',
     user: 'b399df6766fb48',
     password: 'bdb753b4',
     database: 'heroku_69b5684b8aa51e5'
+});*/
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "guara",
+    insecureAuth : true
 });
+
 connection.connect();
 
 module.exports = connection;
--- public/js/authentification/login.js
+++ public/js/authentification/login.js
@@ -1,13 +1,13 @@
 /**** vérifier le nom d'utiisateur****/
-$(document).ready(function () {
-    $('#email').blur(function () {
+$(document).ready(function() {
+    $('#email').blur(function(){
         verefierEmail("email");
     });
 });
 
-/**** vérifier le mot de passe ****/
-$(document).ready(function () {
-    $('#password').blur(function () {
+/**** vérifier le mot de passe ****/ 
+$(document).ready(function() {
+    $('#password').blur(function(){        
         verefirMdp();
     });
 });
@@ -38,36 +38,38 @@ $(document).ready(function() {
         }
       });
   });
+});
 
 
 /**** envoyer un nouveau mot de passe ****/
-function test() {
-    $.post("password", {
-            email: $("#email").val(),
-        },
-        function (data, status) {
-            if (data == 'error') {
-                $('#emailWarning').css("color", "red");
-                $("#emailWarning").text("Envoyer à nouveau");
-            } else if (data == 'NonValide') {
-                $('#emailValide').css("background-color", "red");
-                $('#emailWarning').css("color", "red");
-                $("#emailWarning").text("Compte n'est pas encore validé");
-            } else if (data == 'nonModifier') {
-                $('#emailWarning').css("color", "red");
-                $("#emailWarning").text("Envoyer à nouveau");
-            } else {
-                $('#emailValide').css("background-color", "green");
-                $('#emailWarning').css("color", "green");
-                $("#emailWarning").text("Vérifier votre adresse mail !");
-                $('#mdpOblie').after("<a href='/login' class='text-black text-small'>Connecter</a>");
-            }
-        });
-}
+function test(){
+      $.post("password",
+      {
+        email : $("#email").val(),
+      },
+      function(data, status){
+        if(data == 'error'){
+            $('#emailWarning').css("color","red");
+            $("#emailWarning").text("Envoyer à nouveau");
+        }else if(data == 'NonValide'){
+            $('#emailValide').css("background-color","red");
+            $('#emailWarning').css("color","red");
+            $("#emailWarning").text("Compte n'est pas encore validé");
+        }else if(data == 'nonModifier'){
+            $('#emailWarning').css("color","red");
+            $("#emailWarning").text("Envoyer à nouveau");
+        }else{
+            $('#emailValide').css("background-color","green");
+            $('#emailWarning').css("color","green");
+            $("#emailWarning").text("Vérifier votre adresse mail !");
+            $('#mdpOblie').after("<a href='/login' class='text-black text-small'>Connecter</a>");
+        }
+      });
+  }
 
 
 /**** le formulaire de recupiration d'un mot de passe ****/
-function motDepasseOblie() {
+function motDepasseOblie(){
     $('.remove').remove();
     $('.login').text("Mot de passe oublié");
     $('#login').after("<button name='password' onclick='test()' class='btn btn-primary submit-btn btn-block' id='mdpOblie'>Envoyer</button>");
@@ -75,31 +77,31 @@ function motDepasseOblie() {
 }
 
 
-/**** vérifier le mot de passe ****/
-function verefirMdp() {
-    if ($('#password').val() != "") {
-        if ($('#password').val().length >= 6) {
-            $('#passwordValide').css("background-color", "green");
-        } else {
-            $('#passwordValide').css("background-color", "red");
-            $('#passwordWarning').css("color", "red");
+/**** vérifier le mot de passe ****/ 
+function verefirMdp() {     
+    if($('#password').val() !=""){
+        if($('#password').val().length >= 6){
+            $('#passwordValide').css("background-color","green");
+        }else{
+            $('#passwordValide').css("background-color","red");
+            $('#passwordWarning').css("color","red");
             $("#passwordWarning").text("Le mot de passe est courte");
         }
     }
 }
 
 /**** vérifier l'email ****/
-function verefierEmail(val) {
+function verefierEmail(val){ 
     var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-
-    if ($('#' + val).val() != "") {
-        if (expressionReguliere.test($('#' + val).val())) {
-            $('#' + val + 'Valide').css("background-color", "green");
-            $('#' + val + 'Warning').text("");
-        } else {
-            $('#' + val + 'Valide').css("background-color", "red");
-            $('#' + val + 'Warning').css("color", "red");
-            $('#' + val + 'Warning').text("Email incorrecte");
+        
+    if($('#'+val).val() !=""){
+        if(expressionReguliere.test($('#'+val).val())){
+            $('#'+val+'Valide').css("background-color","green");
+            $('#'+val+'Warning').text("");
+        }else{
+            $('#'+val+'Valide').css("background-color","red");
+            $('#'+val+'Warning').css("color","red");
+            $('#'+val+'Warning').text("Email incorrecte");
         }
     }
 }
